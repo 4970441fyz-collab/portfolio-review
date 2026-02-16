7c3aaa264bde66d64ef25744d044be56cb9ecef9
Anna Ziatkova <firsovaa75594@mail.ru>
2026-02-16 17:30:44 +0300

    fix api module structure

--- app/api/analyze/route.ts
+++ app/api/analyze/route.ts
@@ -1,3 +1,7 @@
-return NextResponse.json({
-  result: "NEW VERSION TEST",
-});
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  return NextResponse.json({
+    result: "NEW VERSION TEST",
+  });
+}
